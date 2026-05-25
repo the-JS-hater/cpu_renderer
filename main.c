21f502adfbbfdcbe6aa3f8642d7c3af49e72cc84
Morgan <mnordberg01@gmail.com>
2026-05-25 20:37:57 +0200

    hello triasngle officially achieved

--- main.c
+++ main.c
@@ -28,7 +28,7 @@ typedef struct
 { Vec2 s, e; } Line;
 
 typedef struct
-{ Vec2 v1, v2, v3; } Triangle;
+{ Vec2 v1, v2, v3; } Triangle2D;
 
 typedef uint32_t Color;
 
@@ -96,7 +96,7 @@ float cross_product2D(Vec2 v, Vec2 u)
 	return v.x*u.y - v.y*u.x; 
 }
 
-void draw_triangle2D(FrameBuffer *frame_buffer, Triangle triangle, Color color)
+void draw_triangle2D(FrameBuffer *frame_buffer, Triangle2D triangle, Color color)
 {
 	int width = frame_buffer->width;
 
@@ -125,6 +125,71 @@ void draw_triangle2D(FrameBuffer *frame_buffer, Triangle triangle, Color color)
 	    if ( (s >= 0) && (t >= 0) && (s + t <= 1))
 	    {
 				unsigned i = (y * width) + x;
+	      frame_buffer->data[i] = color;
+	    }
+	  }
+	}
+}
+
+float cross_product(Vec3 v, Vec3 u)
+{
+	return 0.0f;
+}
+
+void draw_triangle(FrameBuffer *frame_buffer, Vertex *triangle)
+{
+	int width = frame_buffer->width;
+
+	Vec3 v1, v2, v3;
+	v1 = triangle[0].pos;
+	v2 = triangle[1].pos;
+	v3 = triangle[2].pos;
+	Color c1, c2, c3;
+	c1 = triangle[0].color;
+	c2 = triangle[1].color;
+	c3 = triangle[2].color;
+	
+	int max_x = roundf(fmax(v1.x, fmax(v2.x, v3.x)));
+	int min_x = roundf(fmin(v1.x, fmin(v2.x, v3.x)));
+	int max_y = roundf(fmax(v1.y, fmax(v2.y, v3.y)));
+	int min_y = roundf(fmin(v1.y, fmin(v2.y, v3.y)));
+	
+	Vec2 vs1 = {v2.x - v1.x, v2.y - v1.y};
+	Vec2 vs2 = {v3.x - v1.x, v3.y - v1.y};
+
+	for (int x = min_x; x <= max_x; x++)
+	{
+	  for (int y = min_y; y <= max_y; y++)
+	  {
+			Vec2 q = {x - v1.x, y - v1.y};
+	
+	    float s = cross_product2D(q, vs2) / cross_product2D(vs1, vs2);
+	    float t = cross_product2D(vs1, q) / cross_product2D(vs1, vs2);
+	
+	    if ( (s >= 0) && (t >= 0) && (s + t <= 1))
+	    {
+				unsigned i = (y * width) + x;
+				uint8_t r1 = (c1 >> 24) & 0xFF;
+				uint8_t g1 = (c1 >> 16) & 0xFF;
+				uint8_t b1 = (c1 >> 8)  & 0xFF;
+				uint8_t a1 = (c1)       & 0xFF;
+				uint8_t r2 = (c2 >> 24) & 0xFF;
+				uint8_t g2 = (c2 >> 16) & 0xFF;
+				uint8_t b2 = (c2 >> 8)  & 0xFF;
+				uint8_t a2 = (c2)       & 0xFF;
+				uint8_t r3 = (c3 >> 24) & 0xFF;
+				uint8_t g3 = (c3 >> 16) & 0xFF;
+				uint8_t b3 = (c3 >> 8)  & 0xFF;
+				uint8_t a3 = (c3)       & 0xFF;
+				uint8_t r = (1-s-t)*r1 + s*r2 + t*r3;
+				uint8_t g = (1-s-t)*g1 + s*g2 + t*g3;
+				uint8_t b = (1-s-t)*b1 + s*b2 + t*b3;
+				uint8_t a = (1-s-t)*a1 + s*a2 + t*a3;
+				Color color =
+    			(r << 24) |
+    			(g << 16) |
+    			(b << 8)  |
+    			(a);
 	      frame_buffer->data[i] = color;
 	    }
 	  }
@@ -196,14 +261,29 @@ int main()
 		float cx = (float)w_width / 2.0f; 
 		float cy = (float)w_height / 2.0f;
 		float offset = (float)w_height / 4.0f;
-		Triangle t = {
-			{cx, cy - offset},
-			{cx + offset, cy + offset},
-			{cx - offset, cy + offset},
+		// Triangle2D t = {
+		// 	{cx, cy - offset},
+		// 	{cx + offset, cy + offset},
+		// 	{cx - offset, cy + offset},
+		// };
+		// draw_triangle2D(frame_buffer, t, 0x00FF0000);
+		
+		Vertex triangle[3] = {
+			{
+		 		{cx, cy - offset, 1.0f},
+				0xFFFF0000
+			},
+			{
+		 		{cx + offset, cy + offset, 1.0f},
+				0xFF00FF00
+			},
+			{
+		 		{cx - offset, cy + offset, 1.0f},
+				0xFF0000FF
+			},
 		};
+		draw_triangle(frame_buffer, triangle);
 
-		draw_triangle2D(frame_buffer, t, 0x00FF0000);
-	
 		XImage *img = XCreateImage(
   	  display,
   	  visual,
